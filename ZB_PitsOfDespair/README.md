540265d350907da2eba14d8c235f0ffa8898439b
zenblack <40906303+zenblack@users.noreply.github.com>
2024-10-27 21:04:39 -0600

    Following Cam's advice

--- ZB_PitsOfDespair/README.md
+++
@@ -1,3 +0,0 @@
-# Pits-of-Despair
-
-Where the mod readme will be and all the details when WeiDu asks if you want to read it.
--- ZB_PitsOfDespair/data/d/workspace/OHBMORT.d
+++
@@ -1,8 +0,0 @@
-// creator  : weidu (version 24900)
-// argument : OHBMORT.DLG
-// game     : ..
-// source   : ../DATA/BLACKPITS.BIF
-// dialog   : ..\lang\en_us\dialog.tlk
-// dialogF  : (none)
-
-BEGIN ~OHBMORT~
