42cb4f89a59eda6a0be77a1abbc190945124111f
bucketfulofsunshine <135817175+bucketfulofsunshine@users.noreply.github.com>
2024-11-27 21:02:25 +0200

    Add files via upload
    - I made an oopsie before so I'm fixing it.
    - The BP team to Bucket: "Your code is, without a doubt, the worst I have ever run."
    - Bucket: "But it does run." ...Maybe. 
    Actual fixes: 
    - Moved essential Vyantri dialogue to a new d file (it was part of the OPTIONAL interjections d file before). Terrible oversight on my part.
    - Added line in the tp2 so the new dialogue (zbvyan) compiles. 
    - Changed the tp2 lines to refer to the zbpod folder instead of the %mod folder% lines. Since it was mentioned that would break the install. Anyone can revert the changes there if not necessary.

---
+++ zbpod/test/zbvyan.d
@@ -0,0 +1,20 @@
+/* extending Vyatri's dialogue so we can move the players to the new area 
+might not need to append them all but i'm doing it as a failsafe. 
+will need to adjust global numbers to account for having the letter etc - this is just to check if it works... */ 
+EXTEND_BOTTOM TRINNK01 0
++ ~PartyHasItem("zbpdnote") Global("ZBPODSTART","GLOBAL",0)~ + ~I received your note. Has the djinni arrived yet?~ EXTERN TRINNK01 ZBPODMOVE
+END
+
+EXTEND_BOTTOM TRINNK01 1
++ ~PartyHasItem("zbpdnote") Global("ZBPODSTART","GLOBAL",0)~ + ~I received your note. Has the djinni arrived yet?~ EXTERN TRINNK01 ZBPODMOVE
+END
+
+EXTEND_BOTTOM TRINNK01 14
++ ~PartyHasItem("zbpdnote") Global("ZBPODSTART","GLOBAL",0)~ + ~I received your note. Has the djinni arrived yet?~ EXTERN TRINNK01 ZBPODMOVE
+END
+
+CHAIN TRINNK01 ZBPODMOVE
+~I've seen no sign of him. Take a seat, why don't you? I'll bring you something to drink.~
+= ~We even have a bard performing soon, it should make your wait that much easier.~
+DO ~SetGlobal("ZBPODSTART","GLOBAL",1) ClearAllActions() StartCutSceneMode() StartCutScene("podstr")~
+EXIT
