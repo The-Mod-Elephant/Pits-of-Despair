74683185248abcefb35abdbe3833108057254c19
bucketfulofsunshine <135817175+bucketfulofsunshine@users.noreply.github.com>
2024-11-29 12:50:19 +0200

    Add files via upload
    - Re-added intro dialogue line (formerly commented out by Royal due to bugs in standalone). As we have a test file now that shouldn't be a concern anymore. 
    - ...Watch that being buggy again. I'm just saying. It's cursed code. :3 
    - Changed messenger cre to BEST GIRL model. Aka Nephetel. Removed her portrait for this instance to add to her mystery. 
    - Changed messenger lines to new ones (still ongoing as we might add player responses later).
    - Removed 'Vanilla NPC interjections' as a SEPERATE component. It's now part of the core component since we're breaking the standalone BP campaign anyway.
    - HAPPY TESTING EVERYONE! <3

--- zbpod/test/zbmes.d
+++ zbpod/test/zbmes.d
@@ -1,7 +1,8 @@
 BEGIN zbmes
 
 IF ~IsGabber(Player1)~ THEN BEGIN zbmessenger
-SAY ~Have a letter here for you. From some man named Vyantri.~
+SAY ~It looks like I have a letter and a fancy box for you good <PRO_SIRMAAM>.~
+= ~I accept gold or fancy boxes as a delivery fee.~
 IF ~~ DO ~SetGlobal("ZBPodSummonsReceived","GLOBAL",4) AddJournalEntry(@800001,QUEST) GiveItemCreate("zbpdnote",Player1,1,0,0) RevealAreaOnMap("AR2000") DestroySelf()~ EXIT
 END
 
