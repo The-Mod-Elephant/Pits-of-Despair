ccc596d0373b28f7ececf8b98f1d47123269b15c
zenblack <40906303+zenblack@users.noreply.github.com>
2025-03-24 14:31:43 -0600

    Slightly adjusted messenger dialog

--- zbpod/nephetel/dlg/zbmes.d
+++ zbpod/nephetel/dlg/zbmes.d
@@ -2,21 +2,21 @@ BEGIN zbmes
 
 IF ~IsGabber(Player1)~ THEN BEGIN zbmessenger
 SAY ~It looks like I have a letter and a fancy box for you good <PRO_SIRMAAM>.~
-= ~I accept gold or fancy boxes as a delivery fee.~
+= ~I do accept gold or fancy boxes as a delivery fee.~
 + ~PartyGoldGT(0)~ + ~Thank you. Here is a gold piece, don't spend it all in one place.~ + zbmessenger1
 ++ ~Didn't Vytari pay you to bring me this parcel? This must be a Shadow Thief racket, fees for everything.~ + zbmessenger2
 ++ ~Get out of my sight. You were paid to deliver and you have.~ +  zbmessenger3
 END
 
 IF ~~ THEN BEGIN zbmessenger1
-SAY ~Thank you good <PRO_SIRMAAM>, this'll feed my twelve sick children and dying grandmother just fine.~
+SAY ~Thank you, this'll feed my twelve sick children and dying grandmother just fine.~
 IF ~~ DO ~SetGlobal("ZBPodSummonsReceived","GLOBAL",4) 
 GiveItemCreate("zbrrrind",Player1,1,0,0) 
 TakePartyGold(1) ~ EXIT
 END
 
 IF ~~ THEN BEGIN zbmessenger1.5 /* the 5 gold variant */ 
-SAY ~Thank you good <PRO_SIRMAAM>, this'll feed my twelve sick children and dying grandmother just fine.~
+SAY ~Thank you, this'll feed my twelve sick children and dying grandmother just fine.~
 IF ~~ DO ~SetGlobal("ZBPodSummonsReceived","GLOBAL",4) 
 GiveItemCreate("zbrrrind",Player1,1,0,0) 
 TakePartyGold(5)~ EXIT
