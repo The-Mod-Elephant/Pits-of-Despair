5418ec30e4863e662775139f44287b24d20a89d5
dark0dave <dark0dave@mykolab.com>
2025-06-15 11:48:08 +0100

    feat(vanila): Vanila interjections, closes #192
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- zbpod/crossmod/crossmod.d
+++ zbpod/crossmod/crossmod.d
@@ -14,3 +14,23 @@ EXTEND_BOTTOM OHBNAJIM 0
    IF ~Global("BPINBG","GLOBAL",1) Global("ZBNAJIMCROSSMOD","LOCALS",0)~ THEN REPLY @1 /* Najim, I haven't seen you since we escaped Baeloth's Pits. What are you doing here? */ DO ~SetGlobal("ZBNAJIMCROSSMOD","LOCALS",1)~
    EXTERN OHBNAJIM 1
 END
+
+// GLORIOUS BAELOTH CONTENT
+// After Match 2 (this needs to go to dark0dave mod)
+// CHAIN
+// IF ~Global("ZBPOSTMATCH2CROSSMODBANTER1","GLOBAL",0)
+//     Global("ZB_NEPH_POST_INTERJECT_2","GLOBAL",3)
+//     InParty("ZDBAE")
+//     InMyArea("ZDBAE")
+//     !StateCheck("ZDBAE",CD_STATE_NOTVALID)
+//     InParty("Viconia")
+//     InMyArea("Viconia")
+//     !StateCheck("Viconia",CD_STATE_NOTVALID)
+//   ~ THEN ZDBAEJ ZBPOSTMATCH2_1
+//    ~Tis truly a dank and depressed prison they have concocted. These debuts lack the poise and grace of my usual entertainment. How can we expect the crowd to wreath us in flowers after such dour performances?~
+// DO ~SetGlobal("ZBPOSTMATCH2CROSSMODBANTER1","GLOBAL",1)~
+//    == VICONIJ ~A reminder of your ridiculous craft? Where you expecting more from rivvil?~
+//    == ZDBAEJ ~Well, given our present circumstances I can't say I am impressed.~
+//    == VICONIJ ~Even for a male you are insane.~
+//    == ZDBAEJ ~The gifted often are my dear.~
+// EXIT
--- zbpod/dlg/zbvci.d
+++ zbpod/dlg/zbvci.d
@@ -89,7 +89,7 @@ END
 /* the companions interject to stannel's ongoing lies - we're going alphabetically & only adding a few batches per fight. so they don't ALL fire at once */
 
 INTERJECT_COPY_TRANS OHBSTANN 59 ZBSTANNELBATCH1
-== AERIEJ IF ~InParty("Aerie") InMyArea("Aerie") !StateCheck("Aerie",CD_STATE_NOTVALID)~ THEN @3728 /* Why would he say such awful things? He's twisting everything we've done, everything we’re going through, and turning it into something so ugly... so wrong. */
+== AERIEJ IF ~InParty("Aerie") InMyArea("Aerie") !StateCheck("Aerie",CD_STATE_NOTVALID)~ THEN @3728 /* Why would he say such awful things? He's twisting everything we've done, everything we're going through, and turning it into something so ugly... so wrong. */
 == ANOMENJ IF ~InParty("Anomen") InMyArea("Anomen") !StateCheck("Anomen",CD_STATE_NOTVALID)~ THEN @3729 /* This affront to our honor will not stand. Such lies, when allowed to spread, often take on a life of their own. */
 == CERNDJ IF ~InParty("Cernd") InMyArea("Cernd") !StateCheck("Cernd",CD_STATE_NOTVALID)~ THEN @3730 /* These tall tales are like leaves drifting upon the wind. They will scatter in time. */
 == EDWINJ IF ~InParty("Edwin") InMyArea("Edwin") !StateCheck("Edwin",CD_STATE_NOTVALID)~ THEN @3731 /* You think to besmirch my reputation with such drivel? Your envy is as transparent as it is pitiable. */
@@ -138,3 +138,234 @@ INTERJECT_COPY_TRANS OHBSTIRV 7 ZBTRAININGPRE2
   == CERNDJ IF ~InParty("Cernd") InMyArea("Cernd") !StateCheck("Cernd",CD_STATE_NOTVALID) InParty("Nalia") InMyArea("Nalia") !StateCheck("Nalia",CD_STATE_NOTVALID)~ THEN @3754 /* Calmly, dear Nalia, we shall not break as the branches of an oak in a summer storm. */
   == NALIAJ IF ~InParty("Cernd") InMyArea("Cernd") !StateCheck("Cernd",CD_STATE_NOTVALID) InParty("Nalia") InMyArea("Nalia") !StateCheck("Nalia",CD_STATE_NOTVALID)~ THEN @3755 /* I hope you are right. */
 END
+
+// After Match 1
+CHAIN
+IF ~Global("ZBPOSTMATCH1VANILABANTER1","GLOBAL",0)
+    Global("ZB_NEPH_POST_INTERJECT_1","GLOBAL",3)
+    InParty("RASAAD")
+    InMyArea("RASAAD")
+    !StateCheck("RASAAD",CD_STATE_NOTVALID)
+    InParty("Jaheira")
+    InMyArea("Jaheira")
+    !StateCheck("Jaheira",CD_STATE_NOTVALID)
+  ~ THEN RASAADJ ZBPOSTMATCH1_1
+  @3760 /* We are as water in the river flowing to the ocean. Contained by the hands of earth and channeled but inevitable is our freedom. In our journey we cut through the very earth our own path, no matter how it may try to contain us. */
+DO ~SetGlobal("ZBPOSTMATCH1VANILABANTER1","GLOBAL",1)~
+  == JAHEIRAJ @3761 /* I wonder if you can lecture a gladiator into submission. */
+  == RASAADJ @3762 /* If any were to know, it would be you Jaheria. */
+  == JAHEIRAJ @3763 /* Hmph. */
+EXIT
+
+CHAIN
+IF ~Global("ZBPOSTMATCH1VANILABANTER2","GLOBAL",0)
+    Global("ZB_NEPH_POST_INTERJECT_1","GLOBAL",3)
+    InParty("Dorn")
+    InMyArea("Dorn")
+    !StateCheck("Dorn",CD_STATE_NOTVALID)
+    InParty("Korgan")
+    InMyArea("Korgan")
+    !StateCheck("Korgan",CD_STATE_NOTVALID)
+  ~ THEN DORNJ ZBPOSTMATCH1_2
+  @3764 /* Bah, no self respecting orc warband would allow themselves to be taken alive. Grummsh would flay their skin from their weakling bones. */
+DO ~SetGlobal("ZBPOSTMATCH1VANILABANTER2","GLOBAL",1)~
+  == KORGANJ @3765 /* Ye don't have the stones fer that, half-breed. */
+  == DORNJ @3766 /* If you've the guts, Korgan, perhaps this pit will one day host us against each other and I'll shatter YOUR bones! */
+  == KORGANJ @3767 /* Anytime, cragmuncher. */
+EXIT
+
+CHAIN
+IF ~Global("ZBPOSTMATCH1VANILABANTER3","GLOBAL",0)
+    Global("ZB_NEPH_POST_INTERJECT_1","GLOBAL",3)
+    InParty("Mazzy")
+    InMyArea("Mazzy")
+    !StateCheck("Mazzy",CD_STATE_NOTVALID)
+    InParty("Haerdalis")
+    InMyArea("Haerdalis")
+    !StateCheck("Haerdalis",CD_STATE_NOTVALID)
+  ~ THEN DORNJ ZBPOSTMATCH2_1
+  @3768 /* T'was a performance most dizzying! I must confess I feel the beating heart of the arena where the audience's lust for spectacle burns bright. */
+DO ~SetGlobal("ZBPOSTMATCH1VANILABANTER3","GLOBAL",1)~
+  == MAZZYJ @3769 /* Planesman, it should cause you regret to be in the machinations of the foul wizards, not delight. */
+  == HAERDAJ @3770 /* Come now. Do you not feel the pulsing thread of life in this moment? We must capture it's singularity and savor the marrow before it decays before our eyes. */
+  == MAZZYJ @3771 /* A true hero can see this evil for what it is. The virtuous are dishonored in seeking pleasure for killing. */
+  == HAERDAJ @3772 /* I shall leave virtue and honor to you, Tiny Hawk, for who is more deserving? */
+EXIT
+
+// After Match 2
+CHAIN
+IF ~Global("ZBPOSTMATCH2VANILABANTER1","GLOBAL",0)
+    Global("ZB_NEPH_POST_INTERJECT_2","GLOBAL",3)
+    InParty("Jan")
+    InMyArea("Jan")
+    !StateCheck("Jan",CD_STATE_NOTVALID)
+    InParty("Viconia")
+    InMyArea("Viconia")
+    !StateCheck("Viconia",CD_STATE_NOTVALID)
+  ~ THEN VICONIJ ZBPOSTMATCH2_1
+  @3773 /* Twisted experiments, ferocious deadly creatures salivating…you must smell like you wear meat hanging around your neck. Everything is fine, abbil. */
+DO ~SetGlobal("ZBPOSTMATCH2VANILABANTER1","GLOBAL",1)~
+  == JANJ @3774 /* Don't forget the evil weasels. */
+  == VICONIJ @3775 /* What are you blathering about again, you stunted slave? */
+  == JANJ @3776 /* The evil weasels. Great Aunt Petunia always said it was the drow who turned them evil. I had heard they used cursed spider's legs and blood to make them vicious and hungry for fingers to bite. Not my fingers, you evil weasels! */
+  == VICONIJ @3777 /* You truly live in your own tiny world. */
+  == JANJ @3778 /* Did my ears deceive me or is that a short joke? Uncle Scratchy would be proud to see you have adapted to the surface so well. */
+EXIT
+
+CHAIN
+IF ~Global("ZBPOSTMATCH2VANILABANTER2","GLOBAL",0)
+    Global("ZB_NEPH_POST_INTERJECT_2","GLOBAL",3)
+    InParty("Neera")
+    InMyArea("Neera")
+    !StateCheck("Neera",CD_STATE_NOTVALID)
+    InParty("Yoshimo")
+    InMyArea("Yoshimo")
+    !StateCheck("Yoshimo",CD_STATE_NOTVALID)
+  ~ THEN YOSHJ ZBPOSTMATCH2_2
+  @3779 /* Oh, to be a fly on the guildmaster's wall. Even in Thay assassins know of THE Yoshimo and to fear him! */
+DO ~SetGlobal("ZBPOSTMATCH2VANILABANTER2","GLOBAL",1)~
+  == NEERAJ @3780 /* THE Yoshimo? Is that your brother? Ooo, I know, it's your sister, right? */
+  == YOSHJ @3781 /* Come, child, even my family loves my exploits and they will dazzle your already swimming mind! Let's start with the robbery of the Vault of Vapors, oh yes, that one I … */
+  == NEERAJ @3782 /* *yawn* */
+EXIT
+
+CHAIN
+IF ~Global("ZBPOSTMATCH2VANILABANTER3","GLOBAL",0)
+    Global("ZB_NEPH_POST_INTERJECT_2","GLOBAL",3)
+    InParty("Anomen")
+    InMyArea("Anomen")
+    !StateCheck("Anomen",CD_STATE_NOTVALID)
+    InParty("Jaheira")
+    InMyArea("Jaheira")
+    !StateCheck("Jaheira",CD_STATE_NOTVALID)
+  ~ THEN ANOMENJ ZBPOSTMATCH2_3
+  @3783 /* Pah, these beasts are no challenge for a servant of Helm. They are mindless chattel—it is to their masters that justice must be delivered. */
+DO ~SetGlobal("ZBPOSTMATCH2VANILABANTER3","GLOBAL",1)~
+  == JAHEIRAJ @3784 /* Perhaps, Anomen, but there is no shame in cleaning up deserving rabble. Not every battle must test your body and spirit. */
+  == ANOMENJ @3785 /* You don't suggest that we settle for clearing the way so that others may achieve greatness in our stead, do you? We are more than capable of dealing with the fools at the head of this charade. */
+  == JAHEIRAJ @3786 /* These things come only with patience, oh pious one. For now, we bide our time until the moment to strike arrives. */
+EXIT
+
+CHAIN
+IF ~Global("ZBPOSTMATCH2VANILABANTER4","GLOBAL",0)
+    Global("ZB_NEPH_POST_INTERJECT_2","GLOBAL",3)
+    InParty("Jan")
+    InMyArea("Jan")
+    !StateCheck("Jan",CD_STATE_NOTVALID)
+    InParty("Nalia")
+    InMyArea("Nalia")
+    !StateCheck("Nalia",CD_STATE_NOTVALID)
+  ~ THEN NALIAJ ZBPOSTMATCH2_4
+  @3787 /* My uncle once had minotaurs in a maze. They frightened me as a child. */
+DO ~SetGlobal("ZBPOSTMATCH2VANILABANTER4","GLOBAL",1)~
+  == JANJ @3788 /* Don't be afraid, Nalia dear. It hasn't come up, but my great uncle Suladead was an expert maze builder. The rats he put through them couldn't tell their heads from their tails. It was quite a sight to see them stacked on top of one another getting over the walls to find the cheese. */
+  == NALIAJ@3789 /*  You were going somewhere with that? */
+  == JANJ @3790 /* Oh yes, quite right! The best way to escape a maze is to follow your nose, just like a little rat. */
+  == NALIAJ @3791 /* Thanks… I think. */
+EXIT
+
+
+// After Match 3
+CHAIN
+IF ~Global("ZBPOSTMATCH3VANILABANTER1","GLOBAL",0)
+    Global("ZB_NEPH_POST_INTERJECT_3","GLOBAL",3)
+    InParty("Hexxat")
+    InMyArea("Hexxat")
+    !StateCheck("Hexxat",CD_STATE_NOTVALID)
+    InParty("Nalia")
+    InMyArea("Nalia")
+    !StateCheck("Nalia",CD_STATE_NOTVALID)
+  ~ THEN HEXXATJ ZBPOSTMATCH3_1
+  @3792 /* The smell of reptile blood. Disgusting. */
+DO ~SetGlobal("ZBPOSTMATCH3VANILABANTER1","GLOBAL",1)~
+  == NALIAJ @3793 /* I didn't realize you had such a discerning taste, Hexxat. Speaking of which, how do you know which blood will taste good and which will taste bad? */
+  == HEXXATJ @3794 /* By the smell usually, my dear. If it's from a lizard, it's repugnant. */
+  == NALIAJ @3795 /* Okay, but what does it actually smell like? Actually, never mind. I've decided I don't want to know. */
+EXIT
+
+CHAIN
+IF ~Global("ZBPOSTMATCH3VANILABANTER2","GLOBAL",0)
+    Global("ZB_NEPH_POST_INTERJECT_3","GLOBAL",3)
+    InParty("Mazzy")
+    InMyArea("Mazzy")
+    !StateCheck("Mazzy",CD_STATE_NOTVALID)
+    InParty("Valygar")
+    InMyArea("Valygar")
+    !StateCheck("Valygar",CD_STATE_NOTVALID)
+  ~ THEN MAZZYJ ZBPOSTMATCH3_2
+  @3796 /* The nation of Thay is a vile blight upon Faerun. An evil magic empire that festers and rots like a wound. */
+DO ~SetGlobal("ZBPOSTMATCH3VANILABANTER2","GLOBAL",1)~
+  == VALYGARJ @3797 /* Such blighted magic be damned. */
+  == MAZZYJ @3798 /* Of course, brave Valygar. We shall destroy such evil through Arvoreen's will. Delivering justice with blade and virtue. Onward, my friend! */
+EXIT
+
+CHAIN
+IF ~Global("ZBPOSTMATCH3VANILABANTER3","GLOBAL",0)
+    Global("ZB_NEPH_POST_INTERJECT_3","GLOBAL",3)
+    InParty("Keldorn")
+    InMyArea("Keldorn")
+    !StateCheck("Keldorn",CD_STATE_NOTVALID)
+    InParty("Jaheira")
+    InMyArea("Jaheira")
+    !StateCheck("Jaheira",CD_STATE_NOTVALID)
+  ~ THEN JAHEIRAJ ZBPOSTMATCH3_3
+  @3799 /* It gives me no pleasure to have defeated this tribe. Their nature has been twisted to fit the designs of Red Wizards. */
+DO ~SetGlobal("ZBPOSTMATCH3VANILABANTER3","GLOBAL",1)~
+  == KELDORJ @3800 /* They are savages that worship primitive gods that care not a whit for them. They most assuredly deserve the justice we bring. */
+  == JAHEIRAJ @3801 /* Have you no compassion left in those old bones, 'Sir' Keldorn? */
+  == KELDORJ @3802 /* Victory requires we discard the frailties of compassion against evil. Even Harpers know this truth. */
+EXIT
+
+// After Match 4
+CHAIN
+IF ~Global("ZBPOSTMATCH4VANILABANTER1","GLOBAL",0)
+    Global("ZB_NEPH_POST_INTERJECT_4","GLOBAL",3)
+    InParty("Neera")
+    InMyArea("Neera")
+    !StateCheck("Neera",CD_STATE_NOTVALID)
+    InParty("Anomen")
+    InMyArea("Anomen")
+    !StateCheck("Anomen",CD_STATE_NOTVALID)
+  ~ THEN NEERAJ ZBPOSTMATCH4_1
+  @3803 /* You know, one time I actually turned INTO an umber hulk! It was amazing. You could see everywhere with all those eyes and dig! Dig, dig, dig. It felt like eating chocolate. So satisfying. Oh, now I'm hungry. */
+DO ~SetGlobal("ZBPOSTMATCH4VANILABANTER1","GLOBAL",1)~
+  == ANOMENJ @3804 /* When are you not? */
+  == NEERAJ @3805 /* An umber hulk? All the time. See? *waves* */
+  == ANOMENJ @3806 /* The chaos never leaves you, by Helm. You were born umber in your soul. */
+  == NEERAJ @3807 /* Someone's hungry and I'm not sharing any of my snacks. */
+EXIT
+
+CHAIN
+IF ~Global("ZBPOSTMATCH4VANILABANTER2","GLOBAL",0)
+    Global("ZB_NEPH_POST_INTERJECT_4","GLOBAL",3)
+    InParty("Keldorn")
+    InMyArea("Keldorn")
+    !StateCheck("Keldorn",CD_STATE_NOTVALID)
+    InParty("Valygar")
+    InMyArea("Valygar")
+    !StateCheck("Valygar",CD_STATE_NOTVALID)
+  ~ THEN KELDORJ ZBPOSTMATCH4_2
+  @3808 /* Torm has witnessed us triumph on our righteous path to victory. Thay is a loathsome den where evil festers and such creatures must be rooted out whenever the opportunity arises. */
+DO ~SetGlobal("ZBPOSTMATCH4VANILABANTER2","GLOBAL",1)~
+  == VALYGARJ @3809 /* It will do little good—the entirety of Thay should be considered if one wishes to rid evil from our world. */
+  == KELDORJ @3810 /* Be that as it may, Valygar, we can only do what our bodies will allow us. Should the chance arise, still would I hesitate. While evil may lurk in every shadow, there is much that is sacred and worth defending—even in this place. */
+  == VALYGARJ @3811 /* I'll believe what my eyes show me. So far, I'm not impressed. */
+EXIT
+
+CHAIN
+IF ~Global("ZBPOSTMATCH4VANILABANTER3","GLOBAL",0)
+    Global("ZB_NEPH_POST_INTERJECT_4","GLOBAL",3)
+    InParty("Edwin")
+    InMyArea("Edwin")
+    !StateCheck("Edwin",CD_STATE_NOTVALID)
+    InParty("VICONIA")
+    InMyArea("VICONIA")
+    !StateCheck("VICONIA",CD_STATE_NOTVALID)
+  ~ THEN EDWINJ ZBPOSTMATCH4_3
+  @3812 /* Mindless abominations easily defeated by my peerless magical prowess. (Saving this bumbling party, no doubt.) */
+DO ~SetGlobal("ZBPOSTMATCH4VANILABANTER3","GLOBAL",1)~
+  == VICONIJ @3813 /* Are you preening, Edwin? Only the stupidest of males preen as you do. */
+  == EDWINJ @3814 /* In my experience, only women trade barbs like you. (Yes, that should do it.) */
+  == VICONIJ @3815 /* Do not complain of my tongue wizard, it will lash you to ribbons. */
+  == EDWINJ @3816 /* (I think I would enjoy that… ) */
+EXIT
