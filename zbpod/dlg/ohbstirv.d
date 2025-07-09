4e7d6489cf60b3bcffbe6645ddbedc5735660d50
dark0dave <dark0dave@mykolab.com>
2025-07-09 10:18:29 +0100

    fix(striv): Fix for striv cutscene
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- zbpod/dlg/ohbstirv.d
+++ zbpod/dlg/ohbstirv.d
@@ -1,25 +1,25 @@
 BEGIN ~OHBSTIRV~
 
-IF ~Global("ZB_STRIV_TRAINING_CHECK","GLOBAL",1) Global("ZB_STRIV_REWARD_1","GLOBAL",0)~ THEN BEGIN 16
+IF ~Global("ZB_STRIV_REWARD_3","GLOBAL",1)~ THEN BEGIN 0
+  SAY @3051 /* Off with you, then. I got work to do. Daddy's coming, darlings! */
+  IF ~~ THEN EXIT
+END
+
+IF ~Global("ZB_STRIV_TRAINING_CHECK","GLOBAL",1) Global("ZB_STRIV_REWARD_1","GLOBAL",0)~ THEN BEGIN 1
   SAY @3860 /* Hmph, so you lived. Well, how about another match? Some of my bigger puppies are hungry and you look like you got some meat on your bones! */
   IF ~~ THEN REPLY @3862 /* Another match, Stirv. Playtime for the puppies! */ DO ~SetGlobal("ZB_STRIV_REWARD_1","GLOBAL",1)~ GOTO 8
   IF ~~ THEN REPLY @973 /* Not right now. */ DO ~SetGlobal("ZB_STRIV_REWARD_1","GLOBAL",1)~ EXIT
 END
 
-IF ~Global("ZB_STRIV_TRAINING_CHECK", "GLOBAL", 3)  Global("ZB_STRIV_REWARD_3","GLOBAL",0)~ THEN BEGIN 17
+IF ~Global("ZB_STRIV_TRAINING_CHECK", "GLOBAL", 3)  Global("ZB_STRIV_REWARD_3","GLOBAL",0)~ THEN BEGIN 2
   SAY @3861 /* Damn slaves! Dennaton is making me give you a prize for winning against all of my puppies. So here, take it. */
   IF ~~ THEN DO ~SetGlobal("ZB_STRIV_REWARD_3","GLOBAL",1) GiveItemCreate("ZBSTWR",Player1,1,0,0)~ EXIT
 END
 
-IF ~Global("ZB_STRIV_REWARD_3","GLOBAL",1)~ THEN BEGIN 16
-  SAY @3051 /* Off with you, then. I got work to do. Daddy's coming, darlings! */
-  IF ~~ THEN EXIT
-END
-
-IF ~Global("OHB_FINALE","GLOBAL",0) Global("ZB_STRIV_REWARD_3","GLOBAL",0)~
-THEN BEGIN 0
+IF ~Global("OHB_FINALE","GLOBAL",0) Global("ZB_STRIV_REWARD_3","GLOBAL",0) Global("ZB_STRIV_HELLO","GLOBAL", 0)~
+THEN BEGIN 3
   SAY @3038 /* Look at my puppies. Pretty, aren't they? You wanna pet 'em? Take one for a walk? */
-  IF ~~ THEN GOTO 4
+  IF ~~ THEN DO ~SetGlobal("ZB_STRIV_HELLO","GLOBAL",1)~ GOTO 4
 END
 
 IF ~~ THEN BEGIN 4
@@ -32,12 +32,12 @@ IF ~~ THEN BEGIN 5
   IF ~~ THEN GOTO 6
 END
 
-IF ~~ THEN BEGIN 6
+IF ~Global("ZB_STRIV_HELLO","GLOBAL",1)~ THEN BEGIN 6
   SAY @3041 /* You know what my puppies are good at? Fighting. Oh yes, they are. Wanna take one out for a tumble? It'll cost. But don't worry—I won't let the fighting go to the end. Least not theirs! */
   IF ~Global("ohb_stirv_training","global",0)~ THEN REPLY @3042 /* I'll fight your puppies, Stirv. */ GOTO 7
   IF ~Global("ohb_stirv_training","global",1)~ THEN REPLY @3042 /* I'll fight your puppies, Stirv. */ GOTO 8
   IF ~Global("ohb_stirv_training","global",2)~ THEN REPLY @3042 /* I'll fight your puppies, Stirv. */ GOTO 9
-  IF ~~ THEN REPLY @973 /* Not right now. */ GOTO 15
+  IF ~~ THEN REPLY @973 /* Not right now. */ GOTO 10
 END
 
 IF ~~ THEN BEGIN 7
@@ -73,7 +73,7 @@ IF ~~ THEN BEGIN 9
   EXIT
 END
 
-IF ~~ THEN BEGIN 15
+IF ~~ THEN BEGIN 10
   SAY @3051 /* Off with you, then. I got work to do. Daddy's coming, darlings! */
   IF ~~ THEN EXIT
 END
